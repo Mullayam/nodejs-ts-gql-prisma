7a763fc7326379b957d72e4e799a5f5bbd99a699
Mulayam S <mullayam06@gmail.com>
2023-08-06 21:20:56 +0530

    prisma added docker run postgres db setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import prisma from "./lib/db";
 const app = express();
 app.use(express.json())
 
@@ -10,13 +11,25 @@ async function InitializeServer(Port: number = 3300) {
         type Query {
             hello: String
             sayHello(name: String): String
-        }`,
+        }
+        type Mutation {
+            createUser(firstName: String!, lastName: String!,email:String!,password:String!):Boolean
+        }       
+        
+        `,
         resolvers: {
             Query: {
                 hello: () => "Hello World!",
                 sayHello: (parent, { name }) => {
                     return "Hello " + name
                 }
+            },
+            Mutation: {
+                createUser: async (parent, { firstName, lastName, email, password }: { firstName: string, lastName: string, email: string, password: string }) => {
+                    await prisma.user.create({ data: { firstName, lastName, email, password, salt: "random" } })
+                    return true
+                }
+
             }
         }
     })
